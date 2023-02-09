000b5dc245ec3dddd4f0fe201acb4bcb2b2d1637
翔 清水 <k021g1379@g.neec.ac.jp>
2023-02-10 07:39:22 +0900

    05_03おしまい

--- Resources/shaders/OBJPixelShader.hlsl
+++ Resources/shaders/OBJPixelShader.hlsl
@@ -1,7 +1,7 @@
 #include "OBJShaderHeader.hlsli"
 
-Texture2D<float4> tex : register(t0);  // 0番スロットに設定されたテクスチャ
-SamplerState smp : register(s0);      // 0番スロットに設定されたサンプラー
+Texture2D<float4> tex : register(t0); // 0番スロットに設定されたテクスチャ
+SamplerState smp : register(s0); // 0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET
 {
@@ -13,6 +13,32 @@ float4 main(VSOutput input) : SV_TARGET
 	//float4 texcolor = tex.Sample(smp, input.uv);
 	//return float4(texcolor.rgb * shade_color, texcolor.a * m_alpha);
 
-	float4 texcolor = tex.Sample(smp,input.uv);
-	return input.color * texcolor;
+	//float4 texcolor = tex.Sample(smp,input.uv);
+	//return input.color * texcolor;
+	
+	// テクスチャマッピング
+    float4 texcolor = tex.Sample(smp, input.uv);
+	
+	// シェーディングによる色
+    float4 shadecolor;
+	// 光沢度
+    const float shininess = 4.0f;
+	// 頂点から視点への方向ベクトル
+    float3 eyedir = normalize(cameraPos - input.worldpos.xyz);
+	// ライトに部下うベクトルと法線の内積
+    float3 dotlightnormal = dot(lightv, input.normal);
+	// 反射光ベクトル
+    float3 reflect = normalize(-lightv + 2 * dotlightnormal * input.normal);
+	// 環境反射光
+    float3 ambient = m_ambient;
+	// 拡散反射光
+    float3 diffuse = dotlightnormal * m_diffuse;
+	// 鏡面反射光
+    float3 specular = pow(saturate(dot(reflect, eyedir)), shininess) * m_specular;
+	//　全て加算する
+    shadecolor.rgb = (ambient + diffuse + specular) * lightcolor;
+    shadecolor.a = m_alpha;
+	
+	// シェーディングによる色で描画
+    return shadecolor * texcolor;
 }
--- Resources/shaders/OBJVertexShader.hlsl
+++ Resources/shaders/OBJVertexShader.hlsl
@@ -2,63 +2,15 @@
 
 VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOORD)
 {
-	//VSOutput output; // ピクセルシェーダーに渡す値
-	//output.svpos = mul(mat, pos);
-	//output.normal = normal;
-	//output.uv = uv;
-	//return output;
-	
-	////右、下、奥の方向を向いたライト
-	//   float3 lightdir = float3(1, -1, 1);
-	//   lightdir = normalize(lightdir);
-	////ライトの色(白)
-	//   float3 lightcolor = float3(1, 1, 1);
-	
-	//法線にワールド行列によるスケーリング・回転を適用
-    float4 wnormal = normalize(mul(world, float4(normal, 0)));
+    // 法線にワールド行列によるスケーリング・回転を適用
+    float4 wnormal = normalize(mul(world,float4(normal, 0)));
     float4 wpos = mul(world, pos);
-	
-	//-----------------------------------PhoneShader---------------------------//
-	
-	//環境反射光
-    float3 ambient = m_ambient;
-	//拡散反射光
-    //float3 diffuse = dot(-lightdir, normal) * m_diffuse;
-    float3 diffuse = dot(lightcolor, wnormal.xyz) * m_diffuse;
-	
-	//視点座標
-    const float3 eye = float3(0, 0, -20);
-	//光沢度
-    const float shininess = 4.0f;
-	//頂点から始点への方向ベクトル
-    //float3 eyedir = normalize(eye - pos.xyz);
-    float3 eyedir = normalize(cameraPos - wpos.xyz);
-	//反射光ベクトル
-    //float3 reflect = normalize(lightdir + 2 * dot(-lightdir, normal) * normal);
-    float3 reflect = normalize(-lightv + 2 * dot(lightv, wnormal.xyz) * wnormal.xyz);
-	//鏡面反射光
-    float3 specular = pow(saturate(dot(reflect, eyedir)), shininess) * m_specular;
-	
-	//-----------------------------------PhoneShader---------------------------//
-	
-	//ピクセルシェーダーに渡す値
+    
     VSOutput output;
-    //output.svpos = mul(mat, pos);
     output.svpos = mul(mul(viewproj, world), pos);
-	
-	//Lambert反射の計算
-    //output.color.rgb = dot(-lightdir, normal) * m_diffuse * lightcolor;
-    //output.color.a = m_alpha;
-	
-	//-----------------------------------PhoneShader---------------------------//
-	
-	//全て加算する
-    output.color.rgb = (ambient + diffuse + specular) * lightcolor;
-    output.color.a = m_alpha;
-	
-	//-----------------------------------PhoneShader---------------------------//
-	
+    output.worldpos = wpos;
+    output.normal = wnormal.xyz;
     output.uv = uv;
-	
+    
     return output;
 }
