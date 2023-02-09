ea915d0f1d95ef6fc2b057605f20c5d9b94a7e79
ShoShimizu <k021g1379@g.neec.ac.jp>
2023-02-09 17:18:42 +0900

    04_02完成

--- Resources/shaders/OBJPixelShader.hlsl
+++ Resources/shaders/OBJPixelShader.hlsl
@@ -14,5 +14,5 @@ float4 main(VSOutput input) : SV_TARGET
 	//return float4(texcolor.rgb * shade_color, texcolor.a * m_alpha);
 
 	float4 texcolor = tex.Sample(smp,input.uv);
-	return float4(1,1,1,1);
+	return input.color;
 }
