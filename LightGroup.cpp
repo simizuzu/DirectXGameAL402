852210c86112c85b2312a97173a9b586742e09d1
翔 清水 <k021g1379@g.neec.ac.jp>
2023-02-10 09:01:36 +0900

    06_01 23まで

--- LightGroup.cpp
+++ LightGroup.cpp
@@ -1,1 +1,99 @@
 #include "LightGroup.h"
+#include <d3dx12.h>
+
+using namespace DirectX;
+
+/// <summary>
+/// 静的メンバ変数の実態
+/// </summary>
+ID3D12Device* LightGroup::device = nullptr;
+
+void LightGroup::StaticInitialize(ID3D12Device* device)
+{
+	// 再初期化チェック
+	assert(!LightGroup::device);
+
+	// nullptrチェック
+	assert(device);
+
+	LightGroup::device = device;
+}
+
+void LightGroup::Initialize()
+{
+	// nullptrチェック
+	assert(device);
+
+	// ヒーププロパティ
+	CD3DX12_HEAP_PROPERTIES heapProps = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+	// リソース設定
+	CD3DX12_RESOURCE_DESC resourceDesc =
+		CD3DX12_RESOURCE_DESC::Buffer((sizeof(ConstBufferData) + 0xff) & ~0xff);
+
+	HRESULT result;
+	// 定数バッファの生成
+	result = device->CreateCommittedResource(
+		&heapProps, D3D12_HEAP_FLAG_NONE, &resourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr,
+		IID_PPV_ARGS(&constBuff));
+	assert(SUCCEEDED(result));
+
+	// 定数バッファへデータ転送
+	TransferConstBuffer();
+}
+
+void LightGroup::TransferConstBuffer()
+{
+	HRESULT result;
+	//定数バッファへデータ転送
+	ConstBufferData* constMap = nullptr;
+	result = constBuff->Map(0, nullptr, (void**)&constMap);
+	if (SUCCEEDED(result))
+	{
+		constMap->ambientColor = ambientColor;
+
+		for (int i = 0; i < DirLightNum; i++)
+			// ライトが有効なら設定を転送
+		{
+			if (dirLights[i].IsActive())
+			{
+				constMap->dirLights[i].active = 1;
+				constMap->dirLights[i].lightv = -dirLights[i].GetLightDir();
+				constMap->dirLights[i].lightcolor = dirLights[i].GetLightColor();;
+			}
+			// ライトが無効なら転送しない
+			else
+			{
+				constMap->dirLights[i].active = 0;
+			}
+		}
+
+		constBuff->Unmap(0, nullptr);
+	}
+}
+
+void LightGroup::SetAmbientColor(const XMFLOAT3& color)
+{
+	ambientColor = color;
+	dirty = true;
+}
+
+void LightGroup::SetDirLightActive(int index, bool active)
+{
+	assert(0 <= index && index < DirLightNum);
+	dirLights[index].SetActive(active);
+	dirty = true;
+}
+
+void LightGroup::SetDirLightDir(int index, const XMVECTOR& lightdir)
+{
+	assert(0 <= index && index < DirLightNum);
+	dirLights[index].SetLightDir(lightdir);
+	dirty = true;
+}
+
+void LightGroup::SetDirLightColor(int index, const XMFLOAT3& lightcolor)
+{
+	assert(0 <= index && index < DirLightNum);
+	dirLights[index].SetLightColor(lightcolor);
+	dirty = true;
+}
